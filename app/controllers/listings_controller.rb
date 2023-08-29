3def54226705992abf22d1b68260680877b193a4
Zyania Alvarez <zyania.alvarez@gmail.com>
2023-08-29 15:53:39 +0200

    fixing listing controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,10 +1,7 @@
 class ListingsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
   def index
-<<<<<<< HEAD
     @listings = Listing.all
-=======
->>>>>>> f8ec47ad8d18ff4a7e3f6d81cb1c4e41ec9a31d7
   end
 
   # def show
