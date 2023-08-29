f52271b9bf1f19bbae8eb3a74641a51ce53b5bd8
Zyania Alvarez <zyania.alvarez@gmail.com>
2023-08-29 19:48:37 +0200

    listings created :show,  new, create, edit, update // NEED TO FIX : update and create

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,10 +1,43 @@
 class ListingsController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[index show]
+  skip_before_action :authenticate_user!, only: %i[index show new create edit update]
+  before_action :set_listing, only: %i[show edit update]
   def index
     @listings = Listing.all
   end
 
   def show
+    #
+  end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = Listing.new(listing_params)
+    # raise
+    if @listing.save!
+      redirect_to listing_path(@listing)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    #
+  end
+
+  def update
+    @listing.update
+  end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(:title, :location, :price, :description)
+  end
+
+  def set_listing
     @listing = Listing.find(params[:id])
   end
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,7 @@
 class Listing < ApplicationRecord
   belongs_to :user
+  validates :title, uniqueness: { scope: :list_id }
+  validates :location, presence: true
+  validates :price, presence: true
+  validates :description, length: { minimum: 20, maximum: 500, too_short: 'minimum 20 characters', too_long: 'maxiumum 500 characters are allowed' }
 end
