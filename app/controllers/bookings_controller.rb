054543bd02c0fcf7ea8cd7687a0746f842f39e22
Zyania Alvarez <zyania.alvarez@gmail.com>
2023-08-30 16:35:13 +0200

    booking show functional

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,25 +1,30 @@
 class BookingsController < ApplicationController
+  before_action :set_listing, only: %i[show new create]
+
   def show
-    
+    @booking = Booking.find(params[:id])
   end
+
   def new
-    @listing = Listing.find(params[:listing_id])
     @booking = Booking.new
   end
 
   def create
-    @listing = Listing.find(params[:listing_id])
     @booking = Booking.new(booking_params)
     @booking.listing = @listing
     @booking.user = current_user
     if @booking.save
-      redirect_to listing_path(@booking)
+      redirect_to listing_booking_path(@listing, @booking)
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   private
+
+  def set_listing
+    @listing = Listing.find(params[:listing_id])
+  end
 
   def booking_params
     params.require(:booking).permit(:start_date, :end_date, :confirmation)
