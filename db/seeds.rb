21b3bcc26c360b6cc190b641f8bb81cebeaec914
Zyania Alvarez <zyania.alvarez@gmail.com>
2023-08-31 15:22:57 +0200

    repop with new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Booking.destroy_all
 Listing.destroy_all
 User.destroy_all
 
@@ -17,44 +18,59 @@ puts 'Succesful creation'
 
 puts 'Creating 5 fake listings'
 
-Listing.create!(
+listing1 = Listing.new(
   title: 'Little Snow Flake',
   location: 'London',
   price: 75,
   description: 'Cupcake ipsum dolor sit amet. Cotton candy cheesecake sweet roll sweet sesame snaps I love.',
   user: user
 )
+file = URI.open("https://res.cloudinary.com/duewacljk/image/upload/v1693476302/dxq7jbcqwcacz5ahce2f.jpg")
+listing1.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+listing1.save!
 
-Listing.create!(
-  title: 'Cool Whip',
+listing2 = Listing.new(
+  title: 'Buttercup',
   location: 'Rome',
   price: 98,
   description: 'Pie liquorice I love gingerbread chocolate cake icing cake cotton candy I love. Jelly-o cotton candy danish danish fruitcake lemon drops sugar plum lemon drops gummi bears.',
   user: user
 )
+file = URI.open("https://res.cloudinary.com/duewacljk/image/upload/v1693476286/qf3s9u5spmclr05tri25.png")
+listing2.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+listing2.save!
 
-Listing.create!(
+listing3 = Listing.new(
   title: 'Cosmopolitan',
   location: 'Paris',
   price: 50,
   description: 'Brownie caramels jelly-o dessert I love chocolate dessert jelly I love. Chocolate marzipan jelly I love cake I love caramels cupcake. Candy I love pastry I love brownie cake chupa chups.',
   user: user
 )
+file = URI.open("https://res.cloudinary.com/duewacljk/image/upload/v1693476289/ctrbn4ikynxytdxvfz9s.jpg")
+listing3.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+listing3.save!
 
-Listing.create!(
-  title: 'Puff Puff',
+listing4 = Listing.new(
+  title: 'Stardust',
   location: 'Stockholm',
   price: 150,
   description: 'Oat cake biscuit danish gingerbread gummies. Danish dragée jujubes donut chupa chups caramels tootsie roll gingerbread. Ice cream gingerbread croissant cupcake donut gummi bears dessert gingerbread dragée. ',
   user: user
 )
+file = URI.open("https://res.cloudinary.com/duewacljk/image/upload/v1693476304/ayx3cauryeaj2k0otvuc.jpg")
+listing4.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+listing4.save!
 
-Listing.create!(
+listing5 = Listing.new(
   title: 'Bumble Bee',
   location: 'Amsterdam',
   price: 150,
   description: 'Halvah carrot cake carrot cake pudding jelly beans. Sesame snaps powder jujubes chocolate bar marzipan wafer. Pudding biscuit pudding pudding danish pudding.',
   user: user
 )
+file = URI.open("https://res.cloudinary.com/duewacljk/image/upload/v1693476297/zfwv0fjxx5g1kkcrtzvv.jpg")
+listing5.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+listing5.save!
 
-puts '5 fake listings created'
+ puts '5 fake listings created'
